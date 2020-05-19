94d1c2cd2b33ccf5b3542ac57defab978135c1c9
codyc1515 <50791984+codyc1515@users.noreply.github.com>
2020-05-19 19:23:49 +1200

    Support for specifying IP address instead of hostname & detecting when a hostname changes
    Also fixes tabs formatting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inherits = require("util").inherits,
-      moment = require('moment'),
-      ModbusRTU = require("modbus-serial"),
-      dns = require("dns");
+	moment = require('moment'),
+	ModbusRTU = require("modbus-serial"),
+	dns = require("dns");
 
 var client = new ModbusRTU();
 
@@ -20,7 +20,7 @@ module.exports = function(homebridge) {
 function SMAInverter(log, config) {
 	this.log = log;
 	this.hostname = config["hostname"];
-    this.refreshInterval = (config['refreshInterval'] * 60000) || 60000;
+	this.refreshInterval = (config['refreshInterval'] * 60000) || 60000;
 	this.debug = config["debug"] || false;
 
 	this.value = [];
@@ -89,6 +89,9 @@ function SMAInverter(log, config) {
 	};
 	inherits(Characteristic.CustomWatts, Characteristic);
 	Characteristic.CustomWatts.UUID = 'E863F10D-079E-48FF-8F27-9C2605A29F52';
+
+	// Start the connection and refresh cycles
+	this._connect(true);
 }
 
 SMAInverter.prototype = {
@@ -96,6 +99,104 @@ SMAInverter.prototype = {
 	identify: function(callback) {
 		this.log("identify");
 		callback();
+	},
+
+	_connect: function(isInitial) {
+		if(this.debug) {this.log("Connecting to the SMA Inverter (" + this.hostname + ")");}
+
+		// Get the hostname from dns - note: IPv4 support only for now, have not tested IPv6
+		try {
+			dns.resolve(this.hostname, "A", function (err, addresses, family) {
+				// Connect to the ModBus server hostname
+				if(!err) {client.connectTCP(addresses[0]);}
+				else {client.connectTCP(this.hostname);}
+			}.bind(this));
+		}
+		catch(err) {
+			this.log("Failed to resolve DNS hostname of the SMA Inverter (maybe this is an IP address?)", err);
+
+			// Connect to the ModBus server IP address
+			try {client.connectTCP(this.hostname);}
+			catch(err) {throw "Could not connect using ModBus to the SMA Inverter";}
+		}
+
+		try {
+			// Set the ModBus Id to use
+			client.setID(3);
+
+			if(this.debug) {this.log("Connected to the SMA Inverter");}
+		}
+		catch(err) {this.log("Could net set the Channel Number on the SMA Inverter");}
+
+		// Set the automatic refresh onload only
+		if(isInitial) {
+			setInterval(function() {
+				this._refresh();
+			}.bind(this), this.refreshInterval);
+		}
+
+		// Refresh initially once we are connected
+		setTimeout(function() {
+			this._refresh();
+		}.bind(this), 2500);
+	},
+
+	_refresh: function() {
+		if(this.debug) {this.log("Refreshing from SMA Inverter");}
+
+		// Obtain the values
+		try {
+			client.readHoldingRegisters(30051, 10, function(err, data) {
+				switch(data.buffer.readUInt32BE()) {
+					case "8001": this.value.Manufacturer = "SMA Solar Inverters"; break;
+					default: this.value.Manufacturer = "Unknown"; break;
+				}
+			}.bind(this));
+
+			client.readHoldingRegisters(30053, 10, function(err, data) {
+				switch(data.buffer.readUInt32BE()) {
+					case "9319" : this.value.Model = "Sunny Boy 3.0"; break;
+					case "9320" : this.value.Model = "Sunny Boy 3.6"; break;
+					case "9321" : this.value.Model = "Sunny Boy 4.0"; break;
+					case "9322" : this.value.Model = "Sunny Boy 5.0"; break;
+					default: this.value.Model = "Unknown"; break;
+				}
+			}.bind(this));
+
+			client.readHoldingRegisters(30057, 10, function(err, data) {this.value.SerialNumber = data.buffer.readUInt32BE();}.bind(this));
+
+			client.readHoldingRegisters(30775, 10, function(err, data) {
+				// Check if the value is unrealistic (the inverter is not generating)
+				if(data.buffer.readUInt32BE() > 999999) {
+					this.SMAInverter.getCharacteristic(Characteristic.On).updateValue(0);
+					this.SMAInverter.getCharacteristic(Characteristic.OutletInUse).updateValue(0);
+				}
+				else {
+					this.SMAInverter.getCharacteristic(Characteristic.CustomWatts).updateValue(data.buffer.readUInt32BE());
+
+					this.loggingService.addEntry({time: moment().unix(), power: data.buffer.readUInt32BE()});
+
+					if(data.buffer.readUInt32BE() > 0) {
+						this.SMAInverter.getCharacteristic(Characteristic.On).updateValue(1);
+						this.SMAInverter.getCharacteristic(Characteristic.OutletInUse).updateValue(1);
+					}
+					else {
+						this.SMAInverter.getCharacteristic(Characteristic.On).updateValue(0);
+						this.SMAInverter.getCharacteristic(Characteristic.OutletInUse).updateValue(0);
+					}
+
+					client.readHoldingRegisters(30977, 10, function(err, data) {this.SMAInverter.getCharacteristic(Characteristic.CustomAmperes).updateValue(data.buffer.readUInt32BE() / 1000);}.bind(this));
+					client.readHoldingRegisters(30783, 10, function(err, data) {this.SMAInverter.getCharacteristic(Characteristic.CustomVolts).updateValue(data.buffer.readUInt32BE() / 100);}.bind(this));
+					client.readHoldingRegisters(30529, 10, function(err, data) {this.SMAInverter.getCharacteristic(Characteristic.CustomKilowattHours).updateValue(data.buffer.readUInt32BE() / 1000);}.bind(this));
+				}
+			}.bind(this));
+		}
+		catch(err) {
+			this.log("Failed to connect to the SMA Inverter, attempting to reconnect...", err);
+
+			// Attempt to reconnect
+			this._connect();
+		}
 	},
 
 	getServices: function() {
@@ -124,64 +225,6 @@ SMAInverter.prototype = {
 		this.SMAInverter.getCharacteristic(Characteristic.CustomWatts)
 		.on('get', this._getValue.bind(this, "CustomWatts"));
 
-        // Get the hostname from dns - note: IPv4 support only for now, have not tested IPv6
-        dns.resolve(this.hostname, "A", function (err, addresses, family) {
-            // Connect to the ModBus
-            client.connectTCP(addresses[0]);
-    		client.setID(3);
-
-            // Obtain the values
-            setInterval(function() {
-                try {
-        			client.readHoldingRegisters(30051, 10, function(err, data) {
-        				switch(data.buffer.readUInt32BE()) {
-        					case "8001": this.value.Manufacturer = "SMA Solar Inverters"; break;
-        					default: this.value.Manufacturer = "Unknown"; break;
-        				}
-        			}.bind(this));
-
-        			client.readHoldingRegisters(30053, 10, function(err, data) {
-        				switch(data.buffer.readUInt32BE()) {
-        					case "9319" : this.value.Model = "Sunny Boy 3.0"; break;
-        					case "9320" : this.value.Model = "Sunny Boy 3.6"; break;
-        					case "9321" : this.value.Model = "Sunny Boy 4.0"; break;
-        					case "9322" : this.value.Model = "Sunny Boy 5.0"; break;
-        					default: this.value.Model = "Unknown"; break;
-        				}
-        			}.bind(this));
-
-        			client.readHoldingRegisters(30057, 10, function(err, data) {this.value.SerialNumber = data.buffer.readUInt32BE();}.bind(this));
-
-        			client.readHoldingRegisters(30775, 10, function(err, data) {
-                        // Check if the value is unrealistic (the inverter is not generating)
-                        if(data.buffer.readUInt32BE() > 999999) {
-                            this.SMAInverter.getCharacteristic(Characteristic.On).updateValue(0);
-                            this.SMAInverter.getCharacteristic(Characteristic.OutletInUse).updateValue(0);
-                        }
-                        else {
-            				this.SMAInverter.getCharacteristic(Characteristic.CustomWatts).updateValue(data.buffer.readUInt32BE());
-
-            				this.loggingService.addEntry({time: moment().unix(), power: data.buffer.readUInt32BE()});
-
-            				if(data.buffer.readUInt32BE() > 0) {
-            					this.SMAInverter.getCharacteristic(Characteristic.On).updateValue(1);
-            					this.SMAInverter.getCharacteristic(Characteristic.OutletInUse).updateValue(1);
-            				}
-            				else {
-            					this.SMAInverter.getCharacteristic(Characteristic.On).updateValue(0);
-            					this.SMAInverter.getCharacteristic(Characteristic.OutletInUse).updateValue(0);
-            				}
-
-                            client.readHoldingRegisters(30977, 10, function(err, data) {this.SMAInverter.getCharacteristic(Characteristic.CustomAmperes).updateValue(data.buffer.readUInt32BE() / 1000);}.bind(this));
-                            client.readHoldingRegisters(30783, 10, function(err, data) {this.SMAInverter.getCharacteristic(Characteristic.CustomVolts).updateValue(data.buffer.readUInt32BE() / 100);}.bind(this));
-                			client.readHoldingRegisters(30529, 10, function(err, data) {this.SMAInverter.getCharacteristic(Characteristic.CustomKilowattHours).updateValue(data.buffer.readUInt32BE() / 1000);}.bind(this));
-                        }
-        			}.bind(this));
-                }
-                catch(err) {this.log("Failed to connect to the SMA Inverter", err);}
-    		}.bind(this), this.refreshInterval);
-        }.bind(this));
-
 		this.loggingService = new FakeGatoHistoryService("energy", Accessory);
 
 		this.informationService = new Service.AccessoryInformation();
@@ -201,6 +244,7 @@ SMAInverter.prototype = {
 
 	_setValue: function(CharacteristicName, value, callback) {
 		// This does nothing if the user tries to turn it on / off as we cannot action anything on the device
+		if(this.debug) {this.log("SET", CharacteristicName);}
 		callback(null, true);
 	}
 
