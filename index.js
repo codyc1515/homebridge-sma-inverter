4397b2470c06ffe24610d72700e29bdc6b6def56
codyc1515 <50791984+codyc1515@users.noreply.github.com>
2020-11-29 19:35:10 +1300

    Major changes
    * Improved stability
    * Migrates from Switch service to Light Sensor service (shows at the top in iOS 14), as the switch did not do anything

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ module.exports = function(homebridge) {
 
 function SMAInverter(log, config) {
 	this.log = log;
+	this.name = config["name"] || "SMA Solar Inverter";
 	this.hostname = config["hostname"];
-	this.refreshInterval = (config['refreshInterval'] * 60000) || 60000;
+	this.refreshInterval = (config['refreshInterval'] * 1000) || 60000;
 	this.debug = config["debug"] || false;
 
 	this.value = [];
@@ -91,7 +92,12 @@ function SMAInverter(log, config) {
 	Characteristic.CustomWatts.UUID = 'E863F10D-079E-48FF-8F27-9C2605A29F52';
 
 	// Start the connection and refresh cycles
-	this._connect(true);
+	this._connect();
+
+	// Set the automatic refresh onload only
+	setInterval(function() {
+		this._refresh();
+	}.bind(this), this.refreshInterval);
 }
 
 SMAInverter.prototype = {
@@ -102,9 +108,9 @@ SMAInverter.prototype = {
 	},
 
 	_connect: function(isInitial) {
-		if(this.debug) {this.log("Connecting:", this.hostname);}
+		if(this.debug) {this.log("Attempting connection", this.hostname);}
 
-		// Get the hostname from dns - note: IPv4 support only for now, have not tested IPv6
+		// Get the hostname from dns - note: IPv4 support only for currently, have not tested IPv6
 		try {
 			dns.resolve(this.hostname, "A", function (err, addresses, family) {
 				// Connect to the ModBus server hostname
@@ -117,33 +123,23 @@ SMAInverter.prototype = {
 
 			// Connect to the ModBus server IP address
 			try {client.connectTCP(this.hostname);}
-			catch(err) {this.log("Could not connect using ModBus");}
+			catch(err) {this.log("Connection attempt failed");}
 		}
 
 		try {
 			// Set the ModBus Id to use
 			client.setID(3);
 
-			if(this.debug) {this.log("Connected");}
+			if(this.debug) {this.log("Connection successful");}
 		}
 		catch(err) {this.log("Could not set the Channel Number");}
-
-		// Set the automatic refresh onload only
-		if(isInitial) {
-			setInterval(function() {
-				this._refresh();
-			}.bind(this), this.refreshInterval);
-		}
-
-		// Refresh initially once we are connected
-		setTimeout(function() {
-			this._refresh();
-		}.bind(this), 2500);
 	},
 
 	_refresh: function() {
 		// Obtain the values
 		try {
+			/*
+			// Manufacturer
 			client.readHoldingRegisters(30051, 10, function(err, data) {
 				switch(data.buffer.readUInt32BE()) {
 					case "8001": this.value.Manufacturer = "SMA Solar Inverter"; break;
@@ -151,48 +147,56 @@ SMAInverter.prototype = {
 				}
 			}.bind(this));
 
+			// Model
 			client.readHoldingRegisters(30053, 10, function(err, data) {
 				switch(data.buffer.readUInt32BE()) {
 					case "9319" : this.value.Model = "Sunny Boy 3.0"; break;
 					case "9320" : this.value.Model = "Sunny Boy 3.6"; break;
 					case "9321" : this.value.Model = "Sunny Boy 4.0"; break;
 					case "9322" : this.value.Model = "Sunny Boy 5.0"; break;
-					default: this.value.Model = "Sunny Boy Unknown"; break;
+					default: this.value.Model = "Sunny Boy"; break;
 				}
 			}.bind(this));
 
+			// Serial Number
 			client.readHoldingRegisters(30057, 10, function(err, data) {this.value.SerialNumber = data.buffer.readUInt32BE();}.bind(this));
+			*/
 
+			// Currently - Light Sensor
 			client.readHoldingRegisters(30775, 10, function(err, data) {
+				this.lightSensorCurrently.getCharacteristic(Characteristic.CurrentAmbientLightLevel).updateValue(data.buffer.readUInt32BE() / 1000);
+
 				// Check if the value is unrealistic (the inverter is not generating)
-				if(data.buffer.readUInt32BE() > 999999) {
-					if(this.debug) {this.log("Updating status", "Active", "Off (high value)");}
-
-					this.SMAInverter.getCharacteristic(Characteristic.On).updateValue(0);
-					this.SMAInverter.getCharacteristic(Characteristic.OutletInUse).updateValue(0);
-				}
+				if(data.buffer.readUInt32BE() > 999999 && this.debug) {this.log("Device status", "Off - high value");}
 				else {
-					this.SMAInverter.getCharacteristic(Characteristic.CustomWatts).updateValue(data.buffer.readUInt32BE());
-
+					// Eve - Watts
+					this.lightSensorCurrently.getCharacteristic(Characteristic.CustomWatts).updateValue(data.buffer.readUInt32BE());
 					this.loggingService.addEntry({time: moment().unix(), power: data.buffer.readUInt32BE()});
 
-					if(data.buffer.readUInt32BE() > 0) {
-						if(this.debug) {this.log("Updating status", "Active", "On");}
-
-						this.SMAInverter.getCharacteristic(Characteristic.On).updateValue(1);
-						this.SMAInverter.getCharacteristic(Characteristic.OutletInUse).updateValue(1);
-					}
-					else {
-						if(this.debug) {this.log("Updating status", "Active", "Off (low value)");}
-
-						this.SMAInverter.getCharacteristic(Characteristic.On).updateValue(0);
-						this.SMAInverter.getCharacteristic(Characteristic.OutletInUse).updateValue(0);
-					}
-
-					client.readHoldingRegisters(30977, 10, function(err, data) {this.SMAInverter.getCharacteristic(Characteristic.CustomAmperes).updateValue(data.buffer.readUInt32BE() / 1000);}.bind(this));
-					client.readHoldingRegisters(30783, 10, function(err, data) {this.SMAInverter.getCharacteristic(Characteristic.CustomVolts).updateValue(data.buffer.readUInt32BE() / 100);}.bind(this));
-					client.readHoldingRegisters(30529, 10, function(err, data) {this.SMAInverter.getCharacteristic(Characteristic.CustomKilowattHours).updateValue(data.buffer.readUInt32BE() / 1000);}.bind(this));
+					if(this.debug && data.buffer.readUInt32BE() > 0) {this.log("Device status", "On");}
+					else if (this.debug) {this.log("Device status", "Off - low value");}
 				}
+			}.bind(this));
+
+			// Today - Light Sensor
+			client.readHoldingRegisters(30535, 10, function(err, data) {
+				this.lightSensorToday.getCharacteristic(Characteristic.CurrentAmbientLightLevel).updateValue(data.buffer.readUInt32BE() / 1000);
+			}.bind(this));
+
+			// All Time - Light Sensor
+			client.readHoldingRegisters(30529, 10, function(err, data) {
+				this.lightSensorCurrently.getCharacteristic(Characteristic.CustomKilowattHours).updateValue(data.buffer.readUInt32BE() / 1000);
+				this.lightSensorTotal.getCharacteristic(Characteristic.CurrentAmbientLightLevel).updateValue(data.buffer.readUInt32BE() / 1000);
+			}.bind(this));
+
+			// Amperes - FakeGato
+			client.readHoldingRegisters(30977, 10, function(err, data) {
+				this.lightSensorCurrently.getCharacteristic(Characteristic.CustomAmperes).updateValue(data.buffer.readUInt32BE() / 1000);
+			}.bind(this));
+
+			// Volts - FakeGato
+			client.readHoldingRegisters(30783, 10, function(err, data) {
+				this.lightSensorCurrently.getCharacteristic(Characteristic.CustomVolts).updateValue(data.buffer.readUInt32BE() / 100);
 			}.bind(this));
 		}
 		catch(err) {
@@ -204,32 +208,50 @@ SMAInverter.prototype = {
 	},
 
 	getServices: function() {
-		this.SMAInverter = new Service.Outlet(this.name);
 
-		this.SMAInverter.getCharacteristic(Characteristic.On)
-		.on('get',this._getValue.bind(this, "On"))
-		.on('set', this._setValue.bind(this, "On"));
 
-		this.SMAInverter.getCharacteristic(Characteristic.OutletInUse)
-		.on('get', this._getValue.bind(this, "OutletInUse"));
+		this.lightSensorCurrently = new Service.LightSensor(this.name + " Currently", "currently");
+		this.lightSensorCurrently.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
+		.setProps({
+			unit: "kWh"
+		})
+		.on('get',this._getValue.bind(this, "Currently"));
 
-		this.SMAInverter.addCharacteristic(Characteristic.CustomAmperes);
-		this.SMAInverter.getCharacteristic(Characteristic.CustomAmperes)
+		this.lightSensorCurrently.addCharacteristic(Characteristic.CustomAmperes);
+		this.lightSensorCurrently.getCharacteristic(Characteristic.CustomAmperes)
 		.on('get', this._getValue.bind(this, "CustomAmperes"));
 
-		this.SMAInverter.addCharacteristic(Characteristic.CustomKilowattHours);
-		this.SMAInverter.getCharacteristic(Characteristic.CustomKilowattHours)
+		this.lightSensorCurrently.addCharacteristic(Characteristic.CustomKilowattHours);
+		this.lightSensorCurrently.getCharacteristic(Characteristic.CustomKilowattHours)
 		.on('get', this._getValue.bind(this, "CustomKilowattHours"));
 
-		this.SMAInverter.addCharacteristic(Characteristic.CustomVolts);
-		this.SMAInverter.getCharacteristic(Characteristic.CustomVolts)
+		this.lightSensorCurrently.addCharacteristic(Characteristic.CustomVolts);
+		this.lightSensorCurrently.getCharacteristic(Characteristic.CustomVolts)
 		.on('get', this._getValue.bind(this, "CustomVolts"));
 
-		this.SMAInverter.addCharacteristic(Characteristic.CustomWatts);
-		this.SMAInverter.getCharacteristic(Characteristic.CustomWatts)
+		this.lightSensorCurrently.addCharacteristic(Characteristic.CustomWatts);
+		this.lightSensorCurrently.getCharacteristic(Characteristic.CustomWatts)
 		.on('get', this._getValue.bind(this, "CustomWatts"));
 
+
+		this.lightSensorToday = new Service.LightSensor(this.name + " Today", "today");
+		this.lightSensorToday.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
+		.setProps({
+			unit: "kWh"
+		})
+		.on('get',this._getValue.bind(this, "Today"));
+
+
+		this.lightSensorTotal = new Service.LightSensor(this.name + " Total", "total");
+		this.lightSensorTotal.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
+		.setProps({
+			unit: "kWh"
+		})
+		.on('get',this._getValue.bind(this, "Total"));
+
+
 		this.loggingService = new FakeGatoHistoryService("energy", Accessory);
+
 
 		this.informationService = new Service.AccessoryInformation();
 		this.informationService
@@ -238,7 +260,16 @@ SMAInverter.prototype = {
 			.setCharacteristic(Characteristic.Model, this.value.Model)
 			.setCharacteristic(Characteristic.SerialNumber, this.value.SerialNumber);
 
-		return [this.SMAInverter, this.loggingService, this.informationService];
+
+		return [
+			this.lightSensorCurrently,
+			this.lightSensorToday,
+			this.lightSensorTotal,
+			this.loggingService,
+			this.informationService
+		];
+
+
 	},
 
 	_getValue: function(CharacteristicName, callback) {
