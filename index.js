16bc1ab8b2ef65c40de87def300f0d156647b8f1
codyc1515 <50791984+codyc1515@users.noreply.github.com>
2021-12-04 20:35:17 +1300

    Fix number of warnings showing unnecessarily

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function SMAInverter(log, config) {
 			minStep: 0.01,
 			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
 		});
-		this.value = 0;
+		this.value = this.getDefaultValue();
 	};
 	inherits(Characteristic.CustomAmperes, Characteristic);
 	Characteristic.CustomAmperes.UUID = 'E863F126-079E-48FF-8F27-9C2605A29F52';
@@ -56,7 +56,7 @@ function SMAInverter(log, config) {
 			minStep: 0.001,
 			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
 		});
-		this.value = 0;
+		this.value = this.getDefaultValue();
 	};
 	inherits(Characteristic.CustomKilowattHours, Characteristic);
 	Characteristic.CustomKilowattHours.UUID = 'E863F10C-079E-48FF-8F27-9C2605A29F52';
@@ -71,7 +71,7 @@ function SMAInverter(log, config) {
 			minStep: 0.1,
 			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
 		});
-		this.value = 0;
+		this.value = this.getDefaultValue();
 	};
 	inherits(Characteristic.CustomVolts, Characteristic);
 	Characteristic.CustomVolts.UUID = 'E863F10A-079E-48FF-8F27-9C2605A29F52';
@@ -86,7 +86,7 @@ function SMAInverter(log, config) {
 			minStep: 0.1,
 			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
 		});
-		this.value = 0;
+		this.value = this.getDefaultValue();
 	};
 	inherits(Characteristic.CustomWatts, Characteristic);
 	Characteristic.CustomWatts.UUID = 'E863F10D-079E-48FF-8F27-9C2605A29F52';
@@ -172,41 +172,43 @@ SMAInverter.prototype = {
 					this.lightSensorCurrently.getCharacteristic(Characteristic.CustomWatts).updateValue(data.buffer.readUInt32BE());
 					this.loggingService.addEntry({time: moment().unix(), power: data.buffer.readUInt32BE()});
 
-					if(this.debug && data.buffer.readUInt32BE() > 0) {this.log("Device status", "On");}
+					if(data.buffer.readUInt32BE() > 0) {
+						if(this.debug) {this.log("Device status", "On");}
+						
+						// Today - Light Sensor
+						client.readHoldingRegisters(30535, 10, function(err, data) {
+							if(data.buffer.readUInt32BE() > 0 && data.buffer.readUInt32BE() <= (65535*1000) && typeof data.buffer.readUInt32BE() == 'number' && Number.isFinite(data.buffer.readUInt32BE())) {
+								this.lightSensorToday.getCharacteristic(Characteristic.CurrentAmbientLightLevel).updateValue(data.buffer.readUInt32BE() / 1000);
+							}
+						}.bind(this));
+
+						// All Time - Light Sensor
+						client.readHoldingRegisters(30529, 10, function(err, data) {
+							if(data.buffer.readUInt32BE() > 0 && data.buffer.readUInt32BE() <= (65535*1000) && typeof data.buffer.readUInt32BE() == 'number' && Number.isFinite(data.buffer.readUInt32BE())) {
+								this.lightSensorCurrently.getCharacteristic(Characteristic.CustomKilowattHours).updateValue(data.buffer.readUInt32BE() / 1000);
+								this.lightSensorTotal.getCharacteristic(Characteristic.CurrentAmbientLightLevel).updateValue(data.buffer.readUInt32BE() / 1000);
+							}
+						}.bind(this));
+
+						// Amperes - FakeGato
+						client.readHoldingRegisters(30977, 10, function(err, data) {
+							if(data.buffer.readUInt32BE() > 0 && data.buffer.readUInt32BE() <= (65535*1000) && typeof data.buffer.readUInt32BE() == 'number' && Number.isFinite(data.buffer.readUInt32BE())) {
+								this.lightSensorCurrently.getCharacteristic(Characteristic.CustomAmperes).updateValue(data.buffer.readUInt32BE() / 1000);
+							}
+						}.bind(this));
+
+						// Volts - FakeGato
+						client.readHoldingRegisters(30783, 10, function(err, data) {
+							if(data.buffer.readUInt32BE() > 0 && data.buffer.readUInt32BE() <= (65535*100) && typeof data.buffer.readUInt32BE() == 'number' && Number.isFinite(data.buffer.readUInt32BE())) {
+								this.lightSensorCurrently.getCharacteristic(Characteristic.CustomVolts).updateValue(data.buffer.readUInt32BE() / 100);
+							}
+						}.bind(this));
+					}
 					else if (this.debug) {this.log("Device status", "Off - low value");}
 				}
 				else {
 					if(this.debug) {this.log("Device status", "Off - unreasonable value");}
 					this.lightSensorCurrently.getCharacteristic(Characteristic.CurrentAmbientLightLevel).updateValue(0.0001);
-				}
-			}.bind(this));
-
-			// Today - Light Sensor
-			client.readHoldingRegisters(30535, 10, function(err, data) {
-				if(data.buffer.readUInt32BE() > 0 && data.buffer.readUInt32BE() <= (65535*1000) && typeof data.buffer.readUInt32BE() == 'number' && Number.isFinite(data.buffer.readUInt32BE())) {
-					this.lightSensorToday.getCharacteristic(Characteristic.CurrentAmbientLightLevel).updateValue(data.buffer.readUInt32BE() / 1000);
-				}
-			}.bind(this));
-
-			// All Time - Light Sensor
-			client.readHoldingRegisters(30529, 10, function(err, data) {
-				if(data.buffer.readUInt32BE() > 0 && data.buffer.readUInt32BE() <= (65535*1000) && typeof data.buffer.readUInt32BE() == 'number' && Number.isFinite(data.buffer.readUInt32BE())) {
-					this.lightSensorCurrently.getCharacteristic(Characteristic.CustomKilowattHours).updateValue(data.buffer.readUInt32BE() / 1000);
-					this.lightSensorTotal.getCharacteristic(Characteristic.CurrentAmbientLightLevel).updateValue(data.buffer.readUInt32BE() / 1000);
-				}
-			}.bind(this));
-
-			// Amperes - FakeGato
-			client.readHoldingRegisters(30977, 10, function(err, data) {
-				if(data.buffer.readUInt32BE() > 0 && data.buffer.readUInt32BE() <= (65535*1000) && typeof data.buffer.readUInt32BE() == 'number' && Number.isFinite(data.buffer.readUInt32BE())) {
-					this.lightSensorCurrently.getCharacteristic(Characteristic.CustomAmperes).updateValue(data.buffer.readUInt32BE() / 1000);
-				}
-			}.bind(this));
-
-			// Volts - FakeGato
-			client.readHoldingRegisters(30783, 10, function(err, data) {
-				if(data.buffer.readUInt32BE() > 0 && data.buffer.readUInt32BE() <= (65535*100) && typeof data.buffer.readUInt32BE() == 'number' && Number.isFinite(data.buffer.readUInt32BE())) {
-					this.lightSensorCurrently.getCharacteristic(Characteristic.CustomVolts).updateValue(data.buffer.readUInt32BE() / 100);
 				}
 			}.bind(this));
 		}
@@ -219,8 +221,6 @@ SMAInverter.prototype = {
 	},
 
 	getServices: function() {
-
-
 		this.lightSensorCurrently = new Service.LightSensor(this.name + " Currently", "currently");
 		this.lightSensorCurrently.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
 			.setProps({
@@ -228,30 +228,11 @@ SMAInverter.prototype = {
 				minValue: 0,
 				maxValue: 100000,
 				minStep: 0.0001
-			})
-			.on('get',this._getValue.bind(this, "Currently"));
-		this.lightSensorCurrently.setCharacteristic(Characteristic.CurrentAmbientLightLevel, 0);
-
+			});
 		this.lightSensorCurrently.addCharacteristic(Characteristic.CustomAmperes);
-		this.lightSensorCurrently.getCharacteristic(Characteristic.CustomAmperes)
-			.on('get', this._getValue.bind(this, "CustomAmperes"));
-		this.lightSensorCurrently.setCharacteristic(Characteristic.CustomAmperes, 0);
-
 		this.lightSensorCurrently.addCharacteristic(Characteristic.CustomKilowattHours);
-		this.lightSensorCurrently.getCharacteristic(Characteristic.CustomKilowattHours)
-			.on('get', this._getValue.bind(this, "CustomKilowattHours"));
-		this.lightSensorCurrently.setCharacteristic(Characteristic.CustomKilowattHours, 0);
-
 		this.lightSensorCurrently.addCharacteristic(Characteristic.CustomVolts);
-		this.lightSensorCurrently.getCharacteristic(Characteristic.CustomVolts)
-			.on('get', this._getValue.bind(this, "CustomVolts"));
-		this.lightSensorCurrently.setCharacteristic(Characteristic.CustomVolts, 0);
-
 		this.lightSensorCurrently.addCharacteristic(Characteristic.CustomWatts);
-		this.lightSensorCurrently.getCharacteristic(Characteristic.CustomWatts)
-			.on('get', this._getValue.bind(this, "CustomWatts"));
-		this.lightSensorCurrently.setCharacteristic(Characteristic.CustomWatts, 0);
-
 
 		this.lightSensorToday = new Service.LightSensor(this.name + " Today", "today");
 		this.lightSensorToday.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
@@ -260,10 +241,7 @@ SMAInverter.prototype = {
 				minValue: 0,
 				maxValue: 100000,
 				minStep: 0.0001
-			})
-			.on('get',this._getValue.bind(this, "Today"));
-		this.lightSensorToday.setCharacteristic(Characteristic.CurrentAmbientLightLevel, 0);
-
+			});
 
 		this.lightSensorTotal = new Service.LightSensor(this.name + " Total", "total");
 		this.lightSensorTotal.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
@@ -272,13 +250,9 @@ SMAInverter.prototype = {
 				minValue: 0,
 				maxValue: 100000,
 				minStep: 0.0001
-			})
-			.on('get',this._getValue.bind(this, "Total"));
-		this.lightSensorTotal.setCharacteristic(Characteristic.CurrentAmbientLightLevel, 0);
-
+			});
 
 		this.loggingService = new FakeGatoHistoryService("energy", Accessory);
-
 
 		this.informationService = new Service.AccessoryInformation();
 		this.informationService
@@ -287,7 +261,6 @@ SMAInverter.prototype = {
 			.setCharacteristic(Characteristic.Model, this.value.Model)
 			.setCharacteristic(Characteristic.SerialNumber, this.value.SerialNumber);
 
-
 		return [
 			this.lightSensorCurrently,
 			this.lightSensorToday,
@@ -295,8 +268,6 @@ SMAInverter.prototype = {
 			this.loggingService,
 			this.informationService
 		];
-
-
 	},
 
 	_getValue: function(CharacteristicName, callback) {
